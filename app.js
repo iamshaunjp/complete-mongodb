7626f41913d6804a58a3d32bace986880db00f26
Shaun <shaun@thenetninja.co.uk>
2022-03-14 18:13:48 +0000

    lesson-17

--- app.js
+++ app.js
@@ -18,5 +18,16 @@ connectToDb((err) => {
 
 // routes
 app.get('/books', (req, res) => {
-  res.json({mssg: "welcome to the api"})
+  let books = []
+
+  db.collection('books')
+    .find()
+    .sort({author: 1})
+    .forEach(book => books.push(book))
+    .then(() => {
+      res.status(200).json(books)
+    })
+    .catch(() => {
+      res.status(500).json({error: 'Could not fetch the documents'})
+    })
 })
