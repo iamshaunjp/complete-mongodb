ee4d729f921fb3ca943ed1a0d6bacea2dbbee8b8
Shaun <shaun@thenetninja.co.uk>
2022-03-15 09:16:09 +0000

    lesson-21

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { ObjectId } = require('mongodb')
 
 // init app & middleware
 const app = express()
+app.use(express.json())
 
 // db connection
 let db
@@ -50,4 +51,35 @@ app.get('/books/:id', (req, res) => {
     res.status(500).json({error: 'Could not fetch the document'})
   }
 
+})
+
+app.post('/books', (req, res) => {
+  const book = req.body
+
+  db.collection('books')
+    .insertOne(book)
+    .then(result => {
+      res.status(201).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({err: 'Could not create new document'})
+    })
+})
+
+app.delete('/books/:id', (req, res) => {
+
+  if (ObjectId.isValid(req.params.id)) {
+
+  db.collection('books')
+    .deleteOne({ _id: new ObjectId(req.params.id) })
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({error: 'Could not delete document'})
+    })
+
+  } else {
+    res.status(500).json({error: 'Could not delete document'})
+  }
 })
